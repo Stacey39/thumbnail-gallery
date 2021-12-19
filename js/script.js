08bee335524412b50ad87e68183235d0e4c933f6
Stacey39 <ching19880223@gmail.com>
2021-12-20 02:49:46 +0800

    1219

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const lg = document.querySelector(`#lrgimg`)
+const t1 = document.querySelector(`#thumb1`)
+const t2 = document.querySelector(`#thumb2`)
+const t3 = document.querySelector(`#thumb3`)
+
+
+// Get the url string from the target, assigns it to the src of the large image
+const handleThumbClick = function(event) {
+  // Element that was clicked
+  const thumb = event.target
+
+  // The src url from the element
+  const url = thumb.getAttribute(`src`)
+
+  // Assign the strin to the large image
+  lg.setAttribute(`src`, url)
+}
+
+//const getAttribute(`src`,url)
+
+// Add listeners to each thumbnail one by one
+t1.addEventListener(`click`, handleThumbClick)
+t2.addEventListener(`click`, handleThumbClick)
+t3.addEventListener(`click`, handleThumbClick)
